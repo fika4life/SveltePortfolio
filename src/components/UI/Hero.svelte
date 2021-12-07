31aaf8f6f445f05d60950d483fd329d729165aaa
fika4life <fistewie@gmail.com>
2021-12-07 21:00:01 +0100

    hero section overhaul

--- src/components/UI/Hero.svelte
+++ src/components/UI/Hero.svelte
@@ -1,13 +1,31 @@
+<div
+	class="hero min-h-screen"
+	style="background-image: url(&quot;https://source.unsplash.com/DUmFLtMeAbQ/;);"
+>
+	<div class="hero-overlay bg-opacity-60" />
+	<div class="text-center hero-content text-neutral-content">
+		<div class="max-w-md">
+			<h1 class="mb-5 text-5xl font-bold">My Learning Journey</h1>
+			<p class="mb-5">
+				Here you will find a collection of creations and learnings from my dabbling in in web dev
+			</p>
+			<!-- <button class="btn btn-primary">Get Started</button> -->
+		</div>
+	</div>
+</div>
+<!--   
+
 <div class="hero min-h-screen bg-base-200">
 	<div class="flex-col hero-content lg:flex-row-reverse">
-		<img src="https://picsum.photos/id/1005/600/600" class="max-w-sm shadow-2xl" />
+		<img src="https://source.unsplash.com/DUmFLtMeAbQ/w=600" class="max-w-sm shadow-2xl" />
 		<div>
-			<h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+			<h1 class="mb-5 text-5xl font-bold">My Learning Journey</h1>
 			<p class="mb-5">
-				Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-				quasi. In deleniti eaque aut repudiandae et a id nisi.
+				Here you will find a collection of creations and learnings I make along the way as I dabble
+				in web dev, UX/UI design and data analysis.
 			</p>
 			<button class="btn btn-primary">Get Started</button>
 		</div>
 	</div>
 </div>
+ -->
