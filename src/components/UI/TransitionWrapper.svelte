36bda78d0ca1f6b3ad12cc983b79a89341e41662
fika4life <fistewie@gmail.com>
2022-02-04 20:13:20 +0100

    starts page transition component

---
+++ src/components/UI/TransitionWrapper.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { onMount } from 'svelte';
+	import { fly } from 'svelte/transition';
+
+	let show = false;
+
+	onMount(() => {
+		show = true;
+	});
+</script>
+
+<!-- https://youtu.be/UUX5vpwpnrE -->
